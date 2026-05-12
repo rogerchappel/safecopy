4c603cf203ed140e2df6086969b864ab2d373542
Roger Chappel <roger@local>
2026-05-12 18:43:33 +1000

    feat: expose public library API

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { loadConfig } from "./config.js";
+export { createPlan } from "./planner.js";
+export { redactText } from "./redact.js";
+export { pack } from "./pack.js";
+export { inspectBundle, summarizeManifest } from "./inspect.js";
+export { formatPlan } from "./format.js";
+export type * from "./types.js";
